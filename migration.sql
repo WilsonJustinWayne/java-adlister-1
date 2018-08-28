391b8648c7fd1bfe3136ef0f031aa3ade50abde3
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-08-28 14:32:34 -0500

    updated migration sql

--- migration.sql
+++ migration.sql
@@ -6,38 +6,38 @@ DROP TABLE IF EXISTS ad_categories;
 DROP TABLE IF EXISTS categories;
 
 CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+  id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(240) NOT NULL,
+  email    VARCHAR(240) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE ads_categories (
-
-    ad_id INT UNSIGNED NOT NULL,
-    category_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (ad_id) REFERENCES ads(id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-        ON DELETE CASCADE
+  id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id     INT UNSIGNED NOT NULL,
+  title       VARCHAR(240) NOT NULL,
+  description TEXT         NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE categories (
 
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    category VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+  id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  category VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
 
+);
+
+CREATE TABLE ads_categories (
+
+  ad_id       INT UNSIGNED NOT NULL,
+  category_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (ad_id) REFERENCES ads (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+    ON DELETE CASCADE
 );
 
